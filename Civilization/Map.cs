f633ba8857f7d3161ca6be5d8f334ed3fe27080a
levabala <levabala@ya.ru>
2016-11-09 16:58:42 +0300

    v0.1

---
+++ Civilization/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civilization
+{
+    class Map
+    {
+        public Cell[,] map;
+
+        public Map(int width, int height)
+        {
+            map = new Cell[width,height];
+        }
+    }
+}
